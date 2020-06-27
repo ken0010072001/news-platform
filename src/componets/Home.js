e8f399af1365be0461755aba08002d48151e9370
Ken Yeung <ken0010072001@gmail.com>
2020-06-27 20:02:15 +0800

    added Pager and add limit

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState  } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 import map from 'lodash/map'
 import { media } from '../utils/style/'
@@ -7,6 +7,7 @@ import styled from 'styled-components'
 import Card from '../componets/News_card'
 import Header from '../componets/Header'
 import InfiniteScroll from "react-infinite-scroll-component";
+import isEmpty from 'lodash/isEmpty'
 
 
 const CardContainer = styled(InfiniteScroll)`
@@ -22,27 +23,47 @@ const CardContainer = styled(InfiniteScroll)`
   `}
 `
 
+const Loading = styled.div`
+  position: fixed;
+  width: 100%;
+  bottom: 25px;
+  text-align: center;
+
+`
+
 function Home() {
   const dispatch = useDispatch()
   let [articles, setArticles] = useState([])
+  let [hasMore, setHasMore] = useState(true)
+  let pager = useSelector(state => state.pager)
 
-  const apiKey = 'c484092dabfd47a68d6da8903622d49a'
-  
+  //gmail
+  // const apiKey = 'c484092dabfd47a68d6da8903622d49a' 
+   //hotmail
+  const apiKey = '398aaec7c9e843dab54d24151bef6a3d'
+
+  // init the page and get the new when load the page.
   useEffect(() => {
     axios.get(`https://newsapi.org/v2/everything?domains=washingtonpost.com,nytimes.com&apiKey=${apiKey}&pageSize=10&page=1`)
     .then(res => {
       setArticles(res.data.articles)
-      dispatch({
-        type: 'SET_ARTICLES',
-        articles: res.data.articles
-      })
     })
+    if (isEmpty(articles)) {
+      dispatch({
+        type: 'SET_PAGER',
+        pager: pager + 1
+      })
+    }
   },[]);
 
   const fetchMoreData = () => {
-
+    // set for limit for fetch news
+    if (articles.length >= 100) {
+      setHasMore(false)
+      return;
+    }
     setTimeout(() => {
-      axios.get(`https://newsapi.org/v2/everything?domains=washingtonpost.com,nytimes.com&apiKey=${apiKey}&pageSize=10&page=2`)
+      axios.get(`https://newsapi.org/v2/everything?domains=washingtonpost.com,nytimes.com&apiKey=${apiKey}&pageSize=10&page=${pager}`)
       .then(res => {
         articles = [...articles,...res.data.articles ]
         setArticles(articles)
@@ -50,6 +71,11 @@ function Home() {
           type: 'SET_ARTICLES',
           articles: res.data.articles
         })
+      })
+      // after fetch once data, add pager once.
+      dispatch({
+        type: 'SET_PAGER',
+        pager: pager + 1
       })
     }, 1500);
   };
@@ -60,8 +86,8 @@ function Home() {
         <CardContainer className="home-container"
             dataLength={articles.length}
             next={fetchMoreData}
-            hasMore={true}
-            loader={<h4>Loading...</h4>}
+            hasMore={hasMore}
+            loader={<Loading>Loading...</Loading>}
           >
           {
             map(articles, (item, index) => (
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,14 +2,20 @@ import { createStore } from 'redux'
 
 const initialState = {
   articles: [],
+  pager: 1
 }
 
-function newsState(state, action) {
+function newsState(state = initialState, action) {
   switch (action.type) {
     case 'SET_ARTICLES':
       return {
         ...state,
         articles: action.articles
+      }
+    case 'SET_PAGER':
+      return {
+        ...state,
+        pager: action.pager
       }
     default:
       return state
