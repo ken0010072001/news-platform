4294589c1a16986c7095482408cb5b54d765124a
Ken Yeung <ken0010072001@gmail.com>
2020-06-27 16:35:41 +0800

    fix conflict

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -5,8 +5,11 @@ import map from 'lodash/map'
 import { media } from '../utils/style/'
 import styled from 'styled-components'
 import Card from '../componets/News_card'
+import Header from '../componets/Header'
+import InfiniteScroll from "react-infinite-scroll-component";
 
-const HomeContainer = styled.div`
+
+const CardContainer = styled(InfiniteScroll)`
   display: flex;
   flex-wrap: wrap;
   ${media.desktop`
@@ -25,12 +28,12 @@ const HomeContainer = styled.div`
 
 function Home() {
   const dispatch = useDispatch()
-  const [articles, setArticles] = useState({})
+  let [articles, setArticles] = useState([])
 
   const apiKey = 'c484092dabfd47a68d6da8903622d49a'
   
   useEffect(() => {
-    axios.get(`https://newsapi.org/v2/everything?domains=washingtonpost.com,nytimes.com&apiKey=${apiKey}&pageSize=10`)
+    axios.get(`https://newsapi.org/v2/everything?domains=washingtonpost.com,nytimes.com&apiKey=${apiKey}&pageSize=10&page=1`)
     .then(res => {
       setArticles(res.data.articles)
       dispatch({
@@ -40,15 +43,38 @@ function Home() {
     })
   },[]);
 
+  const fetchMoreData = () => {
+
+    setTimeout(() => {
+      axios.get(`https://newsapi.org/v2/everything?domains=washingtonpost.com,nytimes.com&apiKey=${apiKey}&pageSize=10&page=2`)
+      .then(res => {
+        articles = [...articles,...res.data.articles ]
+        setArticles(articles)
+        dispatch({
+          type: 'SET_ARTICLES',
+          articles: res.data.articles
+        })
+      })
+    }, 1500);
+  };
+
   return (
-    <HomeContainer className="home-container">
-      {
-        map(articles, (item, index) => (
-          <Card key={index} article={item}/>
-        ))
-      }
-    </HomeContainer>
-  );
+    <>
+      <Header />
+        <CardContainer className="home-container"
+            dataLength={articles.length}
+            next={fetchMoreData}
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+          >
+          {
+            map(articles, (item, index) => (
+              <Card key={index} article={item}/>
+            ))
+          }
+        </CardContainer>
+    </>
+  )
 }
 
 export default Home;
